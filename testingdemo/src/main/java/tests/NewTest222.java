fb7e88627f0b9d469f157739b9bf0c33c06a2729
Surendra Dire <Surendra.Dire@philips.com>
2022-10-21 11:48:01 +0200

    1

---
+++ testingdemo/src/main/java/tests/NewTest222.java
@@ -0,0 +1,9 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class NewTest222 {
+  @Test
+  public void f() {
+  }
+}
---
+++ testingdemo/src/main/java/testscripts/TestCases.java
@@ -0,0 +1,19 @@
+package testscripts;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class TestCases {
+  @Test
+  public void f() {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
